21cb74fb43151a527c96696cba912fb8638b21be
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-05-03 11:03:20 +0100

    solved the problem in backend now returning to work on the front

--- resources/views/user/create_recipe.blade.php
+++ resources/views/user/create_recipe.blade.php
@@ -55,7 +55,7 @@
         </a>
         <button type="submit" form="recipe-form" class="ml-auto bg-orange-500 hover:bg-orange-600 text-white font-medium py-1.5 px-4 rounded-full">
             Save
-        </button>
+        </button>                         
     </div>
 
     <!-- Form -->
