d3fe84649bea1d53014ec23311d9510c95b64dfa
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-05-05 10:14:18 +0100

    change some stuff in the d$migrations , database

--- database/migrations/2025_05_05_090712_comments.php
+++ database/migrations/2025_05_05_090712_comments.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('recipe_id')->constrained()->onDelete('cascade');
-            $table->foreignId('parent_comment_id')->nullable()->constrained('comments')->onDelete('cascade');
             $table->text('content');
             $table->timestamps();
         });
@@ -27,5 +26,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('comments');
+
     }
 };
