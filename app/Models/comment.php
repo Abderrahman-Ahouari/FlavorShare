e2d7e6adba5422bbc5b5320e0b9fd75cd702ff80
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-05-05 10:19:46 +0100

    updated the models

--- app/Models/comment.php
+++ app/Models/comment.php
@@ -9,7 +9,7 @@ class comment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'recipe_id', 'parent_comment_id', 'content'];
+    protected $fillable = ['user_id', 'recipe_id', 'content'];
 
 
 
@@ -23,15 +23,6 @@ class comment extends Model
         return $this->belongsTo(Recipe::class);
     }
 
-    public function replies()
-    {
-        return $this->hasMany(Comment::class, 'parent_comment_id');
-    }
-
-    public function parentComment()
-    {
-        return $this->belongsTo(Comment::class, 'parent_comment_id');
-    }
 
     public function reactions()
     {
